be0b201e99910683044e289359d8b7f8112c5831
Rohith Vaddepally <rohithvaddepally3@gmail.com>
2025-04-28 12:47:47 +0530

    Update deploy-dev-portal.sh

--- Installer/dev/deploy-dev-portal.sh
+++ Installer/dev/deploy-dev-portal.sh
@@ -9,29 +9,37 @@ if [[ $# -eq 0 ]] || [[ $1 == "" ]]; then
     VERSION_STRING='"version": '
     CURR_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
     NEXT_VERSION=$(echo ${CURR_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-    sed -i '' "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" package.json
+    
+    # Use the correct sed syntax based on the OS
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" package.json
+    else
+      sed -i "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" package.json
+    fi
+    
     echo $NEXT_VERSION
-
     git commit -m "Create new build ${NEXT_VERSION}" -a
     # git push origin
-else
+    else
     CURR_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
     echo $CURR_VERSION
-    sed -i '' '/version/s/[^.]*$/'"$1\",/" package.json
-
+    sed -i '/version/s/[^.]*$/'"$1\",/" package.json
     git commit -m "Create new build 1.0.$1" -a
     # git push origin
-fi
+    fi
+    
+    echo "new_version=${NEXT_VERSION}" >> $GITHUB_OUTPUT
+
 
 npm install
 npm run build 
-gcloud auth application-default set-quota-project test-dev-4321
-gcloud config set project test-dev-4321
+gcloud auth application-default set-quota-project '{{ secrets.DEV_ID }} '
+gcloud config set project '{{ secrets.DEV_ID }} '
 gcloud app deploy --quiet
 
 echo """ """ """ """ """ """ """ """
 
-echo DEPLOYMENT OF  of React Portal is done
+echo React Portal
 
 echo """ """ """ """ """ """ """ """
 
