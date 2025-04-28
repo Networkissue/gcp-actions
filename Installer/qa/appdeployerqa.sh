dc1800e5b42d5d4f9697ac4614a99f9eff55f6ed
Rohith Vaddepally <rohithvaddepally3@gmail.com>
2025-04-28 12:52:39 +0530

    Update appdeployerqa.sh

--- Installer/qa/appdeployerqa.sh
+++ Installer/qa/appdeployerqa.sh
@@ -9,30 +9,42 @@ echo Reverting all local changes
 # git stash
 
 echo replace dev instances with qa instances in app.yaml
+#!/bin/bash
+
+# Check if app.yaml exists
 if [ ! -f app.yaml ]; then 
     echo "app.yaml not found"
     exit 1
 else 
-    sed -i '' '/service_account: test-dev-4321@test-dev-4321.iam.gserviceaccount.com/s/.*/service_account: test-qa-4321@test-qa-4321.iam.gserviceaccount.com/' app.yaml
-    sed -i '' '/  DEPLOYMENT: "dev"/s/.*/  DEPLOYMENT: "qa"/' environments.js
+    # Check the operating system and adjust sed syntax accordingly
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS (uses -i '')
+        sed -i '' '/service_account: test-dev-4321@test-dev-4321.iam.gserviceaccount.com/s/.*/service_account: test-qa-4321@test-qa-4321.iam.gserviceaccount.com/' app.yaml
+        sed -i '' '/  DEPLOYMENT: "dev"/s/.*/  DEPLOYMENT: "qa"/' environments.js
+    else
+        # Linux (uses -i without '')
+        sed -i '/service_account: test-dev-4321@test-dev-4321.iam.gserviceaccount.com/s/.*/service_account: test-qa-4321@test-qa-4321.iam.gserviceaccount.com/' app.yaml
+        sed -i '/  DEPLOYMENT: "dev"/s/.*/  DEPLOYMENT: "qa"/' environments.js
+    fi
 fi
+
 
 
 echo install npm packages
 npm install
 echo set cloud account
-gcloud auth application-default set-quota-project test-qa-4321
-gcloud config set project test-qa-4321
-echo deploy service to appengine
-gcloud app deploy --quiet
-echo deploy service to appengine is done
+gcloud auth application-default set-quota-project '{{ secrets.QA_ID }} '
+# gcloud config set project '{{ secrets.QA_ID }} '
+# echo deploy service to appengine
+# gcloud app deploy --quiet
+# echo deploy service to appengine is done
 
 echo Reverting all QA deployment changes
 # git stash
 # git add .
 # git stash
 
-echo DEPLOYMENT OF NODE SERVICE IS DONE
+echo DEPLOYMENT OF NODE SERVICE
 
 # ------------------------------------------------------------
 # ------------------------------------------------------------
@@ -45,20 +57,21 @@ echo Reverting all local changes
 # git stash
 
 echo replace dev instances with qa instances in app.yaml
-if [ ! -f app.yaml ]; then 
-    echo "app.yaml not found"
-    exit 1
-else 
-    # sed -i "" '/service: test-dev-4321/s/.*/service: test-qa-4321/' app.yaml
+if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' 's/^service_account: .*/service_account: test-qa-4321@test-qa-4321.iam.gserviceaccount.com/' app.yaml
-    sed -i "" '/DEPLOYMENT: /s/.*/    DEPLOYMENT: "qa",/' app.yaml
-    sed -i "" '/DEPLOYMENT: /s/.*/    DEPLOYMENT: "qa",/' environment.js
-    DEMO_URL="https:\/\/my-test-service-dot-test-qa-4321.uc.r.appspot.com/"
+    sed -i '' 's/DEPLOYMENT: .*/DEPLOYMENT: "qa",/' app.yaml
+    sed -i '' 's/DEPLOYMENT: .*/DEPLOYMENT: "qa",/' environment.js
     sed -i '' "s/\(    NODE_URL: \).*/\1\"$DEMO_URL\",/" environment.js
-
-    DEMO_URL="https:\/\/my-test-portal-dot-test-qa-4321.uc.r.appspot.com/"
     sed -i '' "s/\(    REACT_URL: \).*/\1\"$DEMO_URL\",/" environment.js
+else
+    # Linux version (as above)
+    sed -i 's/^service_account: .*/service_account: test-qa-4321@test-qa-4321.iam.gserviceaccount.com/' app.yaml
+    sed -i 's/DEPLOYMENT: .*/DEPLOYMENT: "qa",/' app.yaml
+    sed -i 's/DEPLOYMENT: .*/DEPLOYMENT: "qa",/' environment.js
+    sed -i "s/\(    NODE_URL: \).*/\1\"$DEMO_URL\",/" environment.js
+    sed -i "s/\(    REACT_URL: \).*/\1\"$DEMO_URL\",/" environment.js
 fi
+
 
 
 
@@ -68,15 +81,15 @@ echo prepare build
 npm run build 
 echo set cloud account
 gcloud config set project test-qa-4321
-echo deploy portal to appengine
-gcloud app deploy --quiet
-echo deploy portal to appengine is done
+# echo deploy portal to appengine
+# gcloud app deploy --quiet
+# echo deploy portal to appengine is done
 
-echo Reverting all QA deployment changes
+# echo Reverting all QA deployment changes
 # git stash
 
-echo DEPLOYMENT of QA portal is done
+# echo DEPLOYMENT of QA portal is done
 
 cd ..
 cd Installer
-echo deployment is done
+# echo deployment is done
